33011d2e6ada0658c3e7c4597b3e2326e7a54645
pantojacamilla <pantojacamilla@gmail.com>
2021-02-12 18:24:12 -0300

    Adiciona a empresa selecionada ao Local Storage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 // eslint-disable-next-line import/extensions
 import Empresa from './empresa.js';
 
-// 1º criar um objeto empresa com as informações contidas no select [ok]
-const criaUmObjetoEmpresa = (infoEmpresa) => {
+const criaUmObjetoEmpresa = (empresaSelecionada) => {
   let empresa = {};
-  if (infoEmpresa === 0) {
+  if (empresaSelecionada === 0) {
     empresa = new Empresa('F S S COMERCIO VAREJISTA DE COMB E LUBRIFICANTES LTDA', '07.533.992/0001-61');
   } else {
     empresa = new Empresa('ALESAT COMBUSTIVEIS S.A.', '23.314.594/0035-50');
@@ -12,11 +11,16 @@ const criaUmObjetoEmpresa = (infoEmpresa) => {
   return empresa;
 };
 
-// Guardar o objeto empresa no local storage
+const adicionaEmpresaAoLocalStorage = (objetoEmpresa) => {
+  window.localStorage.setItem('empresa', JSON.stringify(objetoEmpresa));
+};
 
+// Botão enviar é clicado
 document.querySelector('#selecao-empresa').addEventListener('submit', (e) => {
   e.preventDefault();
-  const infoEmpresa = parseInt(document.querySelector('#empresa').value, 10);
-  console.log(criaUmObjetoEmpresa(infoEmpresa));
-  window.open('relatorio.html');
+  const empresaSelecionada = parseInt(document.querySelector('#empresa').value, 10);
+  const obejtoEmpresa = criaUmObjetoEmpresa(empresaSelecionada);
+  adicionaEmpresaAoLocalStorage(obejtoEmpresa);
+  // window.open('relatorio.html');
+  // window.location.replace('relatorio.html');
 });
