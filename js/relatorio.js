81b242bd4a6f9476765d1cdb14bef8951d19b521
pantojacamilla <pantojacamilla@gmail.com>
2021-02-12 19:18:14 -0300

    Mostra a empresa selecionada em relatorio.html

--- js/relatorio.js
+++ js/relatorio.js
@@ -1,12 +1,30 @@
-class Relatorio {
-  numeroSequencial;
-  //empresa empresa
-  //produduto: Produto
-  valorTotalPresumido;
-  diferencaEntreValorPresumidoEValorDeVenda;
-  icmsASerRestituido;
-  valorTotalPresumidoNoMes;
-  valorTotalPraticadoNoMes;
-  diferencaEntreOTotalPresumidoEOPraticado;
-  valorDeIcmsASerDistituidoNoMes;
-}
+// class Relatorio {
+//   numeroSequencial;
+//   //empresa empresa
+//   //produduto: Produto
+//   valorTotalPresumido;
+//   diferencaEntreValorPresumidoEValorDeVenda;
+//   icmsASerRestituido;
+//   valorTotalPresumidoNoMes;
+//   valorTotalPraticadoNoMes;
+//   diferencaEntreOTotalPresumidoEOPraticado;
+//   valorDeIcmsASerDistituidoNoMes;
+// }
+
+const mostraNomeDaEmpresaNaTela = (nomeEmpresa) => {
+  const divNome = document.querySelector('#nome-empresa');
+  divNome.innerHTML = nomeEmpresa;
+};
+
+const mostraCnpjDaEmpresaNaTela = (cnpj) => {
+  const divCnpj = document.querySelector('#cnpj-empresa');
+  divCnpj.innerHTML = cnpj;
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const objetoEmpresa = JSON.parse(window.localStorage.getItem('empresa'));
+  const { nomeEmpresa } = objetoEmpresa;
+  const { cnpj } = objetoEmpresa;
+  mostraNomeDaEmpresaNaTela(nomeEmpresa);
+  mostraCnpjDaEmpresaNaTela(cnpj);
+});
