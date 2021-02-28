e6de7839ce43c359633bf510334b918b8a4b9417
pantojacamilla <pantojacamilla@gmail.com>
2021-02-28 16:49:10 -0300

    Implementa funcao que retorna os numeros com 4 casas decimais

--- js/Produto.js
+++ js/Produto.js
@@ -2,9 +2,20 @@ export default class Produto {
   constructor(nomeDoProduto, qtdVendidaDoProduto,
     valorDaUnidadeDoProduto, valorTotalVendidoDoProduto) {
     this.nomeDoProduto = nomeDoProduto; // <xProd>
-    this.qtdVendidaDoProduto = parseFloat((qtdVendidaDoProduto)); // <qCom>
-    this.valorDaUnidadeDoProduto = parseFloat((valorDaUnidadeDoProduto)); // <vUnCom>
-    this.valorTotalVendidoDoProduto = parseFloat(valorTotalVendidoDoProduto);
+    this.qtdVendidaDoProduto = retornaOValorTruncado(qtdVendidaDoProduto); // <qCom>
+    this.valorDaUnidadeDoProduto = retornaOValorTruncado(valorDaUnidadeDoProduto); // <vUnCom>
+    this.valorTotalVendidoDoProduto = retornaOValorTruncado(valorTotalVendidoDoProduto);
     // <vProd>
   }
+
+
+  retornaOValorTruncado(valor) {
+    const pontoCaractere = '.';
+    const indexDoPonto = valor.indexOf(pontoCaractere);
+    const qtdNumerosUtilizados = (indexDoPonto + 5);
+    const valorTruncado = valor.substr(0, qtdNumerosUtilizados);
+
+    return parseFloat(valorTruncado);
+  };
+
 }
