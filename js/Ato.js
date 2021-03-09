6364757691ac45b1888af57cdd2110cde49870af
pantojacamilla <pantojacamilla@gmail.com>
2021-03-09 19:23:06 -0300

    Adiciona opcao de recebimento das datas no Ato

--- js/Ato.js
+++ js/Ato.js
@@ -3,6 +3,8 @@ export default class Ato {
     this.numeroAto = numeroAto;
     this.dataInicio = new Date(`${dataInicio}T00:00:00-03:00`);
     this.dataFim = new Date(`${dataFim}T00:00:00-03:00`);
+    // this.dataInicio = new Date(new Date(`${dataInicio}T00:00:00-03:00`).toDateString());
+    // this.dataFim = new Date(new Date(`${dataFim}T00:00:00-03:00`).toDateString());
     this.produtoImposto = produtoImposto;
     this.ano = ano;
   }
