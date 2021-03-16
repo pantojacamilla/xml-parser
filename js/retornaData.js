cb5fcaee68a391faf13cab5a9938ec855549b45e
pantojacamilla <pantojacamilla@gmail.com>
2021-03-16 12:35:57 -0300

    Reescreve o concerto do bug data

--- js/retornaData.js
+++ js/retornaData.js
@@ -2,9 +2,9 @@ const retornaData = (data) => {
   let dataCorreta;
 
   if (data.includes('T')) {
-    dataCorreta = new Date(new Date(data).toString());
+    dataCorreta = new Date(new Date(data).toDateString());
   } else {
-    dataCorreta = new Date(new Date(`${data}T00:00:00-03:00`).toString());
+    dataCorreta = new Date(new Date(`${data}T00:00:00-03:00`).toDateString());
   }
 
   return dataCorreta;
