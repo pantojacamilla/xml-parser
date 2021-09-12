f850350f888aecb750765cf2e983af38e286154f
pantojacamilla <pantojacamilla@gmail.com>
2021-09-12 15:50:27 -0300

    Implementa criaçao do objeto empresa

--- js/Empresa.js
+++ js/Empresa.js
@@ -11,4 +11,34 @@ export default class Empresa {
     this.nomeFantasia = nomeFantasia;
     this.tipoEmpresa = tipoEmpresa; // matriz ou filial
   }
+
+  static retornaObjetoEmpresa(empresaSelecionada) {
+    let cnpj;
+    let nomeEmpresarial;
+    let nomeFantasia;
+    let tipoEmpresa;
+
+    if (empresaSelecionada == 1) {
+      cnpj = '07.533.992/0001-61';
+      nomeEmpresarial = 'A F S S COMERCIO VAREJISTA DE COMBUSTIVEIS E LUBRIFICANTES LTDA';
+      nomeFantasia = 'POSTO SAO JERONIMO';
+      tipoEmpresa = 'Matriz';
+    }
+
+    if (empresaSelecionada == 2) {
+      cnpj = '07.533.992/0002-42';
+      nomeEmpresarial = 'A F S S COMERCIO VAREJISTA DE COMBUSTIVEIS E LUBRIFICANTES LTDA';
+      nomeFantasia = 'POSTO KM 29';
+      tipoEmpresa = 'Filial';
+    }
+
+    if (empresaSelecionada == 3) {
+      cnpj = '23.314.594/0035-50';
+      nomeEmpresarial = 'ALESAT COMBUSTIVEIS S.A.';
+      nomeFantasia = 'ALESAT COMBUSTIVEIS S.A.';
+      tipoEmpresa = 'Filial';
+    }
+
+    return new Empresa(cnpj, nomeEmpresarial, nomeFantasia, tipoEmpresa);
+  }
 }
--- js/index.js
+++ js/index.js
@@ -1,15 +1,5 @@
 import Empresa from './Empresa.js';
 
-// const criaUmObjetoEmpresa = (empresaSelecionada) => {
-//   let empresa = {};
-//   if (empresaSelecionada === 0) {
-//     empresa = new Empresa('F S S COMERCIO VAREJISTA DE COMB E LUBRIFICANTES LTDA', '07.533.992/0001-61');
-//   } else {
-//     empresa = new Empresa('ALESAT COMBUSTIVEIS S.A.', '23.314.594/0035-50');
-//   }
-//   return empresa;
-// };
-
 // const adicionaEmpresaAoLocalStorage = (objetoEmpresa) => {
 //   window.localStorage.setItem('empresa', JSON.stringify(objetoEmpresa));
 // };
@@ -18,8 +8,10 @@ import Empresa from './Empresa.js';
 document.querySelector('#selecao-empresa').addEventListener('submit', (e) => {
   e.preventDefault();
   const empresaSelecionada = document.querySelector('#empresa').value;
+  const obejtoEmpresa = Empresa.retornaObjetoEmpresa(empresaSelecionada);
 
-  // const obejtoEmpresa = criaUmObjetoEmpresa(empresaSelecionada);
+  console.log(obejtoEmpresa);
+
   // adicionaEmpresaAoLocalStorage(obejtoEmpresa);
   // // window.open('relatorio.html');
   // window.location.replace('./relatorio.html');
