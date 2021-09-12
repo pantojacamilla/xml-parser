996599e1aeb3fc9bb4a385adc682a09dfcb0101c
pantojacamilla <pantojacamilla@gmail.com>
2021-09-12 15:55:52 -0300

    Adiciona objeto empresa ao local storage

--- js/Empresa.js
+++ js/Empresa.js
@@ -1,10 +1,4 @@
 export default class Empresa {
-  //   Informações iniciais da empresa:
-  // . CNPJ
-  // . Tipo empresa: Matriz/Filial
-  // . Nome empresarial
-  // . Nome fantasia
-
   constructor(cnpj, nomeEmpresarial, nomeFantasia, tipoEmpresa) {
     this.cnpj = cnpj;
     this.nomeEmpresarial = nomeEmpresarial;
--- js/index.js
+++ js/index.js
@@ -1,18 +1,14 @@
 import Empresa from './Empresa.js';
 
-// const adicionaEmpresaAoLocalStorage = (objetoEmpresa) => {
-//   window.localStorage.setItem('empresa', JSON.stringify(objetoEmpresa));
-// };
+const adicionaEmpresaAoLocalStorage = (objetoEmpresa) => {
+  window.localStorage.setItem('empresa', JSON.stringify(objetoEmpresa));
+};
 
 // Botão enviar é clicado
 document.querySelector('#selecao-empresa').addEventListener('submit', (e) => {
   e.preventDefault();
   const empresaSelecionada = document.querySelector('#empresa').value;
   const obejtoEmpresa = Empresa.retornaObjetoEmpresa(empresaSelecionada);
-
-  console.log(obejtoEmpresa);
-
-  // adicionaEmpresaAoLocalStorage(obejtoEmpresa);
-  // // window.open('relatorio.html');
-  // window.location.replace('./relatorio.html');
+  adicionaEmpresaAoLocalStorage(obejtoEmpresa);
+  window.location.replace('./relatorio.html');
 });
