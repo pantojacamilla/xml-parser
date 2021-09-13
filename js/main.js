71935b11e6942675a75fc91a3fde4e87f622e91d
pantojacamilla <pantojacamilla@gmail.com>
2021-09-13 20:35:46 -0300

    Refatora a conversao de FileList em array

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-const fileListInputNotasFiscais = document.getElementById('inputNotasFiscais');
-let i;
-
 // Início do Programa
-document.querySelector('#inputNotasFiscais').addEventListener('change', (e) => {
-  const arrayNotasFiscais = [];
-
-  // Transforma o FileFist em um array
-  for (i = 0; i < fileListInputNotasFiscais.files.length; i += 1) {
-    arrayNotasFiscais.push(fileListInputNotasFiscais.files[i]);
-  }
+document.querySelector('#inputNotasFiscais').addEventListener('change', (event) => {
+  const arrayNotasFiscais = Array.from(event.target.files);
 });
