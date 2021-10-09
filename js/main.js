943377aca13969b292a53c1eb32a96cec38dbd7f
pantojacamilla <pantojacamilla@gmail.com>
2021-10-09 20:09:30 -0300

    Verifica se o arquivo pertense a empresa selecionada

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 // Início do Programa
+
+// Acess the CNPJ in local storage here
+const empresa = JSON.parse(window.localStorage.getItem('empresa'));
+
+const retornaSoOsNumerosDoCNPJ = () => {
+  const regexCNPJ = /[!"#$%& '() * +,-./: ;<=>?@[\]^ `{|}~]/g;
+  return empresa.cnpj.replace(regexCNPJ, '');
+};
+
+// Esse arquivo pertence a essa empresa e portanto deve ser considerado?
+const isFileValid = (nomeArquivo) => {
+  // Does it include the "_" (this might be used to validate if a NF is -inu OR NOT)
+  // Does it include the CNPJ on it?
+
+  const cnpj = retornaSoOsNumerosDoCNPJ();
+  const retorno = (nomeArquivo.includes(cnpj)) ? 'true' : 'false';
+  return retorno;
+};
+
 document.querySelector('#inputNotasFiscais').addEventListener('change', (event) => {
-  const arrayNotasFiscais = Array.from(event.target.files);
+  const arquivos = Array.from(event.target.files);
+  arquivos.forEach((arquivo, index) => {
+    console.log(`${arquivo.name} ${isFileValid(arquivo.name)} N:${index}`);
+  });
 });
